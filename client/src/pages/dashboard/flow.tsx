a343a1f2c621a1943bca3d7eef513a5237b7d8c0
Amir Nakhaei <6696894+amirrr@users.noreply.github.com>
2023-10-21 01:42:41 +0200

    update platform flow

---
+++ client/src/pages/dashboard/flow.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import React, { useState, useCallback } from "react";
+import ReactFlow, {
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  applyEdgeChanges,
+  applyNodeChanges,
+} from "reactflow";
+
+import PromptNode from "./promptnode";
+
+import "reactflow/dist/style.css";
+
+const rfStyle = {
+  backgroundColor: "#dedede",
+};
+
+const initialNodes = [
+  {
+    id: "1",
+    type: "PromptNode",
+    position: { x: 0, y: 0 },
+    data: {
+      selects: {
+        "handle-0": "default",
+      },
+    },
+  },
+];
+
+const nodeTypes = { PromptNode: PromptNode };
+
+function Flow() {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState([]);
+
+  const onNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    [setNodes]
+  );
+  const onEdgesChange = useCallback(
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    [setEdges]
+  );
+  const onConnect = useCallback(
+    (connection) => setEdges((eds) => addEdge(connection, eds)),
+    [setEdges]
+  );
+
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        nodeTypes={nodeTypes}
+        fitView
+        style={rfStyle}
+      >
+        <Controls />
+        <MiniMap />
+        <Background variant="dots" gap={12} size={1} />
+      </ReactFlow>
+    </div>
+  );
+}
+
+export default Flow;
