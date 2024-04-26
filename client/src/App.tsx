5f7b8f70fe10636a39113bb85fbcb0459c7e47d9
amirrr <6696894+amirrr@users.noreply.github.com>
2024-04-26 21:38:06 +0200

    Add @types/react-router-dom and react-router-dom@6.23.0 dependencies

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,25 @@
 import Dashboard from "./components/Builder/Dashboard";
 import WorkflowProvider from "./context/WorkflowProvider";
 import { ReactFlowProvider } from "reactflow";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 function App() {
   return (
     <>
       <ReactFlowProvider>
         <WorkflowProvider>
-          <div>
-            <Dashboard />
-          </div>
+          <Router>
+            <ul>
+              <li>
+                <Link to="/dashboard">
+                  <Dashboard />
+                </Link>
+              </li>
+              <li>
+                <Link to="/table">Zillow Group</Link>
+              </li>
+            </ul>
+          </Router>
         </WorkflowProvider>
       </ReactFlowProvider>
     </>
