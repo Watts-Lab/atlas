7aa7dd61c1e64f5276e7c0e3a408995e11665e43
amirrr <6696894+amirrr@users.noreply.github.com>
2024-06-02 01:01:28 +0200

    chore: Refactor Condition Name feature for better readability

--- server/features/condition/condition_name.py
+++ server/features/condition/condition_name.py
@@ -2,17 +2,21 @@
 Condition name feature. This feature is responsible for returning the name of the conditions in the experiment.
 """
 
-from ..gpt_feature import GPTFeature
+from server.features.gpt_feature import GPTFeature
 
 
 class Feature(GPTFeature):
     """
-    Condition name feature. This feature is responsible for returning the name of the conditions in the experiment.
+    Condition name feature. This feature is responsible for
+    returning the name of the conditions in the experiment.
     """
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         print("Condition Name Feature initialized.", args, kwargs)
 
-    def display(self):
+    def display(self) -> None:
+        """
+        Display method for the Condition Name class.
+        """
         print("This is the Condition Name class.")
