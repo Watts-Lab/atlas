2e5aec85ee07f658478999848c0434c871843fc3
amirrr <6696894+amirrr@users.noreply.github.com>
2024-06-09 18:22:40 +0200

    fix vitest config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,13 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
-import { defineConfig } from 'vite'
+import { defineConfig as defineViteConfig, mergeConfig } from 'vite'
+import { defineConfig as defineVitestConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+const viteConfig = defineViteConfig({
   plugins: [react()],
+})
+
+const vitestConfig = defineVitestConfig({
   test: {
     coverage: {
       provider: 'v8',
@@ -14,3 +16,5 @@ export default defineConfig({
     setupFiles: './tests/setup.js',
   },
 })
+
+export default mergeConfig(viteConfig, vitestConfig)
