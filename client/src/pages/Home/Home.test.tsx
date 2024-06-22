986c21742a3d248347da4e5d3b80c926e1ba61f1
amirrr <6696894+amirrr@users.noreply.github.com>
2024-06-22 21:29:22 +0200

    fix test

--- client/src/pages/Home/Home.test.tsx
+++ client/src/pages/Home/Home.test.tsx
@@ -1,10 +1,15 @@
 import { describe, it, expect } from 'vitest'
 import { render } from '@testing-library/react'
+import { BrowserRouter as Router } from 'react-router-dom'
 import Home from './Home'
 
 describe('Home', () => {
   it('should render Home component', () => {
-    const { getByTestId } = render(<Home />)
+    const { getByTestId } = render(
+      <Router>
+        <Home />
+      </Router>,
+    )
     const home = getByTestId('home-h1')
     expect(home).toBeTruthy()
   })
