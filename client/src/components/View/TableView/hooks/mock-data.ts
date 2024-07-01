8b173618361202c2417b84139643d680238f452d
amirrr <6696894+amirrr@users.noreply.github.com>
2024-07-01 20:47:30 +0200

    add default data from common sense paper

--- client/src/components/View/TableView/hooks/mock-data.ts
+++ client/src/components/View/TableView/hooks/mock-data.ts
@@ -2,89 +2,36 @@ import { Result } from './data-handler'
 
 // Sample usage
 export const check_data: Result = {
-  file_name: 'Workflow-1',
+  file_name:
+    'whiting-watts-2024-a-framework-for-quantifying-individual-and-collective-common-sense.pdf',
   experiments: [
     {
-      experiment_name: 'Experiment 1',
-      experiment_description: 'Description 1',
       conditions: [
         {
-          condition_name: 'Condition 1',
-          condition_description: 'Description 1',
-          condition_type: 'Type 1',
-          condition_message: 'Message 1',
           condition_behaviors: [
             {
-              behavior_name: 'Behavior 1',
-              behavior_description: 'Description 1',
-              behavior_priority: 'Type 1',
-              behavior_focal: 'Message 1',
+              behavior_description: 'Participant agreement level with claims',
+              behavior_focal: 'focal',
+              behavior_name: 'Agreement',
+              behavior_priority: 'primary',
             },
-          ],
-        },
-        {
-          condition_name: 'Condition 2',
-          condition_description: 'Description 2',
-          condition_type: 'Type 2',
-          condition_message: 'Message 2',
-          condition_behaviors: [
             {
-              behavior_name: 'Behavior 2',
-              behavior_description: 'Description 2',
-              behavior_priority: 'Type 2',
-              behavior_focal: 'Message 2',
+              behavior_description: 'Prediction about majority agreement',
+              behavior_focal: 'not focal',
+              behavior_name: 'Prediction',
+              behavior_priority: 'primary',
             },
           ],
-        },
-        {
-          condition_name: 'Condition 3',
-          condition_description: 'Description 3',
-          condition_type: 'Type 3',
-          condition_message: 'Message 3',
-          condition_behaviors: [
-            {
-              behavior_name: 'Behavior 3',
-              behavior_description: 'Description 3',
-              behavior_priority: 'Type 3',
-              behavior_focal: 'Message 3',
-            },
-          ],
+          condition_description:
+            'Participants evaluated given claims based on whether they agreed with the claim and predicted if the majority would also agree.',
+          condition_message: 'No',
+          condition_name: 'Baseline Evaluation',
+          condition_type: 'control',
         },
       ],
-    },
-    {
-      experiment_name: 'Experiment 2',
-      experiment_description: 'ExpDescription 2',
-      conditions: [
-        {
-          condition_name: 'exp2 Condition 1',
-          condition_description: 'exp2 Description 1',
-          condition_type: 'exp2 Type 1',
-          condition_messalsge: 'exp2 Message 1',
-          condition_behaviors: [
-            {
-              behavior_name: '--',
-              behavior_description: '--',
-              behavior_priority: '--',
-              behavior_focal: '--',
-            },
-          ],
-        },
-        {
-          condition_name: 'exp2 Condition 2',
-          condition_description: 'exp2 Description 2',
-          condition_type: 'exp2 Type 2',
-          condition_message: 'exp2 Message 2',
-          condition_behaviors: [
-            {
-              behavior_name: '--',
-              behavior_description: '--',
-              behavior_priority: '--',
-              behavior_focal: '--',
-            },
-          ],
-        },
-      ],
+      experiment_description:
+        'This study investigates the properties and distribution of common sense knowledge among individuals and groups. It defines commonsensicality for individual claims and people, and quantifies common sense as a clique detection problem on a bipartite belief graph of people and claims.',
+      experiment_name: 'Quantifying Commonsensicality',
     },
   ],
 }
