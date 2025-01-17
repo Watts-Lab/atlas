aae7038c88235e970796cb41a8a2cb717da006b1
Amirhossein Nakhaei <6696894+amirrr@users.noreply.github.com>
2025-01-17 21:28:42 +0330

    Fix deployment (#106)
    * fix: standardize string quotes and remove unnecessary whitespace in components
    
    * feat: add step to print file tree in test workflow
    
    * feat: update import paths and add utility function for class name merging
    
    * add docker testing to workflows
    
    * feat: update AWS deploy workflow to trigger on Docker test completion and add permissions for id-token and contents
    
    * fix: remove docker push commands from test workflow to streamline image building
    
    * refactor: update docker image build steps in test workflow for clarity

--- client/src/components/ui/collapsible.tsx
+++ client/src/components/ui/collapsible.tsx
@@ -1,6 +1,4 @@
-"use client"
-
-import * as CollapsiblePrimitive from "@radix-ui/react-collapsible"
+import * as CollapsiblePrimitive from '@radix-ui/react-collapsible'
 
 const Collapsible = CollapsiblePrimitive.Root
 
--- client/src/components/ui/progress.tsx
+++ client/src/components/ui/progress.tsx
@@ -1,9 +1,7 @@
-"use client"
+import * as React from 'react'
+import * as ProgressPrimitive from '@radix-ui/react-progress'
 
-import * as React from "react"
-import * as ProgressPrimitive from "@radix-ui/react-progress"
-
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils'
 
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
@@ -12,13 +10,13 @@ const Progress = React.forwardRef<
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
-      "relative h-2 w-full overflow-hidden rounded-full bg-neutral-900/20 dark:bg-neutral-50/20",
-      className
+      'relative h-2 w-full overflow-hidden rounded-full bg-neutral-900/20 dark:bg-neutral-50/20',
+      className,
     )}
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-neutral-900 transition-all dark:bg-neutral-50"
+      className='h-full w-full flex-1 bg-neutral-900 transition-all dark:bg-neutral-50'
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
