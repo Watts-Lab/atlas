b6c31dd4ec840ec770e3d241ce922f1611577cea
amirrr <6696894+amirrr@users.noreply.github.com>
2025-01-17 20:41:55 +0330

    update imports

--- client/src/components/ui/sidebar.tsx
+++ client/src/components/ui/sidebar.tsx
@@ -5,7 +5,7 @@ import { VariantProps, cva } from 'class-variance-authority'
 import { PanelLeft } from 'lucide-react'
 
 import { useIsMobile } from '@/hooks/use-mobile'
-import { cn } from '@/lib/utils'
+import { cn } from '../../lib/utils'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Separator } from '@/components/ui/separator'
